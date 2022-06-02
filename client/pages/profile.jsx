de9bfac21fe5583ea3f2822dd43c322e2a22603e
Irwin Chen <irwin.t.chen@gmail.com>
2022-06-02 19:33:47 +0000

    updated client view, added descripton and user name

--- client/pages/profile.jsx
+++ client/pages/profile.jsx
@@ -3,9 +3,16 @@ import React from 'react';
 export default function Profile(props) {
   const [targetProfile] = props.targetProfile;
   return (
-    <div>
-      <div className="w-19/20">
-        <img className="aspect-[10/11] object-cover w-full" src={`./images/${targetProfile.imageUrl}`}></img>
+    <div className="flex flex-col items-center">
+      <div className="w-19/20 mb-2">
+        <img className="aspect-[10/11] object-cover w-full shadow-md rounded-lg" src={`./images/${targetProfile.imageUrl}`}></img>
+      </div>
+      <div className="profile-description w-19/20 bg-white shadow-md rounded-lg">
+        <p className="text-3xl font-bold p-2">{targetProfile.userName}</p>
+        <p className="p-2">{targetProfile.userDescription}</p>
+      <div className="profile-tags w-19/20 bg-white shadow-md rounded-lg">
+
+      </div>
       </div>
     </div>
   );
--- database/data.sql
+++ database/data.sql
@@ -5,7 +5,7 @@ insert into "tags" ("label")
 
 insert into "user" ("userName","userDescription", "imageUrl")
      values ('Nana and Popo', 'ice climbers', 'image-iceclimbers.webp'),
-            ('Geodude', 'used rockclimb, it was super effective!', 'image-geodude.jpeg'),
+            ('Geodude', 'Geodude is a brownish-gray boulder Pokémon. It has bulging, rocky eyebrows, trapezoidal, brown eyes, and a wide mouth. Its arms are muscular with five-fingered hands. Geodude uses its arms to climb steadily up steep mountain paths. Geodude used rockclimb! It was super effective!', 'image-geodude.jpeg'),
             ('Fish', 'hmmm', 'image-fish.jpeg'),
             ('Cat', 'YAMERO', 'image-cat.jpeg');
 
